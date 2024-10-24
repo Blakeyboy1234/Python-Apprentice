b1b946b655c5ef97258511fed811cd86c1b1ba74
Blakeyboy1234 <blake.moser@students.jointheleague.org>
2024-10-24 22:47:20 +0000

    blake

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -11,18 +11,18 @@ window.
 """
 
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup(width=600, height=600)     # Set the size of the window
-
+turtle.setup(width=600, height=600)     # Set the size of the wind
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.speed(0)
+tina.shape('triangle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast.
 
 tina.pencolor('blue')                   # Set the pen color to blue
 tina.forward(150)                       # Move tina forward by the forward distance
 tina.left(90)                           # Turn tina left by the left turn
 
-tina.pencolor('red')                    # Set the pen color to red
+tina.pencolor('blue')                    # Set the pen color to red
 tina.forward(150)                       # Continuie the last two steps three more times
 tina.left(90)                           # to draw a square
 
@@ -43,10 +43,14 @@ tina.backward(20)                       # Move tina backward by 20
 
 tina.goto(-50,0)
 tina.pendown()
-tina.color('red')                       # Set the color of tina to red
-tina.begin_fill()
-tina.circle(101, steps=50)
-tina.end_fill()
+tina.color('red') 
+for i in range(4):
+    for y in range(5):
+        tina.circle(100, steps=4)
+        tina.forward(10)
+    tina.right(90)
+    tina.forward(1)
+
 
 turtle.exitonclick()                    # Close the window when we click on it
 
