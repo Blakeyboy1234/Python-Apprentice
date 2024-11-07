4baddcf732eca0d7ce092f6d5d5dfc635a3e6f01
Blakeyboy1234 <blake.moser@students.jointheleague.org>
2024-11-07 23:47:24 +0000

    blake

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -14,24 +14,24 @@ import turtle                           # Tell Python we want to work with the t
 turtle.setup(width=600, height=600)     # Set the size of the wind
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-tina.speed(0)
+tina.speed(10)
 tina.shape('triangle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast.
+tina.speed(1)                           # Make the turtle move as fast, but not too fast.
 
 tina.pencolor('blue')                   # Set the pen color to blue
 tina.forward(150)                       # Move tina forward by the forward distance
-tina.left(90)                           # Turn tina left by the left turn
+tina.right(80)                           # Turn tina left by the left turn
 
 tina.pencolor('blue')                    # Set the pen color to red
-tina.forward(150)                       # Continuie the last two steps three more times
-tina.left(90)                           # to draw a square
+tina.forward(100)                       # Continuie the last two steps three more times
+tina.right(90)                           # to draw a square
 
-tina.pencolor('green')                  # Set the pen color to green
-tina.forward(150)
+tina.pencolor('purple')                  # Set the pen color to green
+tina.forward(100)
 tina.left(90)
 
 tina.pencolor('purple')                 # Set the pen color to purple
-tina.forward(150)
+tina.forward(100)
 tina.left(90)
 
 tina.penup()                            # Lift the pen up so we can move tina without drawing
@@ -43,13 +43,13 @@ tina.backward(20)                       # Move tina backward by 20
 
 tina.goto(-50,0)
 tina.pendown()
-tina.color('red') 
+tina.color('green') 
 for i in range(4):
     for y in range(5):
-        tina.circle(100, steps=4)
+        tina.circle(100, steps=5)
         tina.forward(10)
     tina.right(90)
-    tina.forward(1)
+    tina.forward(3)
 
 
 turtle.exitonclick()                    # Close the window when we click on it
